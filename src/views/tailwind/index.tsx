25f16c607e7f6c8be6a12d08b64fca8b1ff7813f
10years-ago <414578531@qq.com>
2021-09-15 20:54:51 +0800

    2021-09-15-prettierrc

--- src/views/tailwind/index.tsx
+++ src/views/tailwind/index.tsx
@@ -5,6 +5,8 @@ import { Menu } from 'components/menu'
 import React, { useState } from 'react'
 import { useQuery } from 'urql'
 import Link from 'next/link'
+import Highlight, { defaultProps } from 'prism-react-renderer'
+import theme from 'prism-react-renderer/themes/nightOwl'
 
 export const Tailwind: React.FC = () => {
   const [titleId, setTitleId] = useState('')
@@ -14,66 +16,127 @@ export const Tailwind: React.FC = () => {
     query: getLangByLangName,
     variables: {
       langName: 'tailwind',
-      titleId
-    }
+      titleId,
+    },
   })
-  const { data, fetching, error } = result;
+  const { data, fetching, error } = result
+  const exampleCode = `
+  import React, { useState } from "react";
+  
+  function Example() {
+    const [count, setCount] = useState(0);
+  
+    return (
+      <div>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>
+          Click me
+        </button>
+      </div>
+    );
+  } 
+  `.trim()
   return (
     <Layout lang='tailwind'>
       <div className='flex relative h-full'>
         {/* 左边菜单栏 */}
-        {
-          data?.langByLangName && (<Menu lang={data.langByLangName} titleId={titleId} setTitleId={setTitleId} />)
-        }
+        {data?.langByLangName && (
+          <Menu
+            lang={data.langByLangName}
+            titleId={titleId}
+            setTitleId={setTitleId}
+          />
+        )}
         {/* 右边主要内容 */}
         <div className='flex-1'>
           <div className='w-full max-w-4xl mx-auto'>
-            {
-              data?.langByLangName?.contents.map(item => {
-                return (
-                  <div key={item.id}>
-                    <p className='font-bold text-3xl'>{item?.contentTitle}</p>
-                    <p>{item?.content}</p>
-                    <p>{item?.code}</p>
-                    <input type="text" value={code} onInput={(e) => setCode(e.currentTarget.value)}/>
-                    <div dangerouslySetInnerHTML={{ __html: code }}></div>
-                  </div>
-                )
-              })
-            }
+            {data?.langByLangName?.contents.map((item) => {
+              return (
+                <div key={item.id}>
+                  <p className='font-bold text-3xl'>{item?.contentTitle}</p>
+                  <p>{item?.content}</p>
+                  <p>{item?.code}</p>
+                  <Highlight
+                    {...defaultProps}
+                    code={exampleCode}
+                    theme={theme}
+                    language='jsx'
+                  >
+                    {({
+                      className,
+                      style,
+                      tokens,
+                      getLineProps,
+                      getTokenProps,
+                    }) => (
+                      <pre
+                        className={`${className} text-left my-4 mx-0 overflow-scroll p-2`}
+                        style={style}
+                      >
+                        {tokens.map((line, i) => (
+                          <div
+                            className='table-row'
+                            {...getLineProps({ line, key: i })}
+                          >
+                            <span className='table-cell text-right pr-4 opacity-50 select-none'>
+                              {i + 1}
+                            </span>
+                            <div className='table-cell'>
+                              {line.map((token, key) => (
+                                <span {...getTokenProps({ token, key })} />
+                              ))}
+                            </div>
+                          </div>
+                        ))}
+                      </pre>
+                    )}
+                  </Highlight>
+                  <input
+                    type='text'
+                    value={code}
+                    onInput={(e) => setCode(e.currentTarget.value)}
+                  />
+                  <div dangerouslySetInnerHTML={{ __html: code }}></div>
+                </div>
+              )
+            })}
           </div>
         </div>
         {/* playground按钮 */}
         <div className={`${!playground ? 'hidden' : 'absolute w-full h-full'}`}>
-          <iframe src='https://play.tailwindcss.com/' className='w-full h-full'/> 
+          <iframe
+            src='https://play.tailwindcss.com/'
+            className='w-full h-full'
+          />
           <div onClick={() => setPlayground(false)}>
-            <Avatar 
-              src='kita.jpg' 
-              type='circle' 
+            <Avatar
+              src='kita.jpg'
+              type='circle'
               className='absolute w-16 h-16 right-8 top-16'
             />
           </div>
         </div>
-        <div className={`${playground ? 'hidden' : ''}`} onClick={() => setPlayground(true)}>
-          <Avatar 
-            src='kita.jpg' 
-            type='circle' 
+        <div
+          className={`${playground ? 'hidden' : ''}`}
+          onClick={() => setPlayground(true)}
+        >
+          <Avatar
+            src='kita.jpg'
+            type='circle'
             className='absolute w-16 h-16 right-8 top-8'
           />
         </div>
-        {
-          !playground && (
-            <Link href='https://tailwindcomponents.com/gradient-generator/'>
-              <a target='_blank'>
-                <Avatar 
-                  src='kita.jpg' 
-                  type='circle' 
-                  className='absolute w-16 h-16 right-8 top-32'
-                />
-              </a>
-            </Link>
-          )
-        }
+        {!playground && (
+          <Link href='https://tailwindcomponents.com/gradient-generator/'>
+            <a target='_blank'>
+              <Avatar
+                src='kita.jpg'
+                type='circle'
+                className='absolute w-16 h-16 right-8 top-32'
+              />
+            </a>
+          </Link>
+        )}
       </div>
     </Layout>
   )
